b7b8bcd17c8b656abbec4f1e38f61639b4b7bb12
nsh0919 <chelsea4570@naver.com>
2026-02-11 18:40:43 +0900

    [Feat] 로그인 유지 기능 추가

--- devicelife-api/src/main/java/com/devicelife/devicelife_api/domain/user/dto/AuthDto.java
+++ devicelife-api/src/main/java/com/devicelife/devicelife_api/domain/user/dto/AuthDto.java
@@ -1,9 +1,6 @@
 package com.devicelife.devicelife_api.domain.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 public class AuthDto {
@@ -72,6 +69,9 @@ public class AuthDto {
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+=-]*$",
                 message = "비밀번호는 영문과 숫자를 포함해야 하고 8~20자여야합니다.")
         String password;
+
+        @NotNull
+        Boolean keepLogin;
     }
 
     @Builder
